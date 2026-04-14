a367116384d6b607995c25889ceb872780679d36
Boris Amah <soft@SoftGuest.softguest.com>
2026-04-14 12:18:58 +0100

    next fix verify

--- app/[locale]/verify-email/page.tsx
+++ app/[locale]/verify-email/page.tsx
@@ -41,34 +41,31 @@ export default function VerifyEmailPage() {
     
           <div className="relative w-full max-w-md p-6">
     
-            {/* Header */}
+            {/* Glass Card */}
+            <div className="backdrop-blur-xl bg-white/10 border border-white/20 shadow-2xl rounded-2xl p-6 space-y-4">
+                 {/* Header */}
             <div className="text-center mb-6">
               <div className="inline-flex items-center justify-center w-14 h-14 bg-green-700/80 backdrop-blur-md rounded-2xl mb-3 shadow-lg">
                 <Leaf className="w-7 h-7 text-yellow-300" />
               </div>
     
-                  <div className="min-h-screen flex items-center justify-center">
+                  <div className="flex justify-center">
                     <form onSubmit={handleVerify} className="space-y-4">
-                        <h1>Verify Email</h1>
-
+                        <h1 className="text-2xl font-bold text-white">Verify Email</h1>
                         <input
                         type="text"
                         placeholder="Enter code"
                         value={code}
                         onChange={(e) => setCode(e.target.value)}
-                        className="border p-2"
+                        className="border p-2 rounded w-full bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-green-500"
                         />
 
                         {error && <p className="text-red-500">{error}</p>}
 
-                        <button className="bg-green-500 text-white py-2 px-4 rounded" type="submit">Verify</button>
+                        <button className="bg-green-500 text-white py-2 px-6 rounded cursor-pointer" type="submit">Verify</button>
                     </form>
                     </div>
             </div>
-    
-            {/* Glass Card */}
-            <div className="backdrop-blur-xl bg-white/10 border border-white/20 shadow-2xl rounded-2xl p-6 space-y-4">
-
     
             </div>
           </div>
